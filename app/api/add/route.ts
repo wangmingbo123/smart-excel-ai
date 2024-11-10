160ba911b48b9d91a623a261360b0ff83b725d97
wangmingbo123 <1445260526@qq.com>
2024-11-10 08:23:00 +0800

    images

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -59,6 +59,7 @@ export async function POST(request: Request) {
       selfIntroduction:body.selfIntroduction,
       email:body.email,
       phone:body.phone,
+      images:body.images
     }
     const res = await prisma.interviewer.upsert({
       where: { name: name },
